d9e81b47c54368273f2cd38d06ad1297b9f79b7f
Austin Moore <119389763+ALmoOorE@users.noreply.github.com>
2024-10-17 12:58:23 +0000

    done

--- src/Ch01/01_04/CodeDemo.cpp
+++ src/Ch01/01_04/CodeDemo.cpp
@@ -4,13 +4,45 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main(){
-    std::string str;
-    std::cout << "Enter your name: " << std::flush;
-    std::cin >> str;
-    std::cout << "Nice to meet you, " << str << "!" << std::endl;
 
-    std::cout << std::endl << std::endl;
-    return (0);
+int row;
+int col;
+char sym;
+
+cout<<"How many rows do you want? ";
+cin>>row;
+
+cout<<"How many columns do you want? ";
+cin>>col;
+
+cout<<"What character do you want? ";
+cin>>sym;
+
+for(int i=1;i<=row;i++){
+    for(int j=1;j<=col;j++){
+        cout<<sym;
+    }
+    cout<<sym<<'\n';
+}
+
+
+
+
+   /* for(int i = 1;i<=3;i++){
+
+        for(int j = 1;j <= 10;j++){
+        cout<<j<<" ";
+    }
+    cout<<'\n';
+    }
+    */
+    
+
+
+
+    return 0;
+   
 }
